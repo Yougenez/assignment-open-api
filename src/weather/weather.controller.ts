733cd1650836f254ee9641887067642be4de73cd
Yougenez <Yougenez>
2025-01-11 17:27:24 +0800

    feat: complete currentweather

---
+++ src/weather/weather.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { WeatherService } from './weather.service';
+
+@Controller('weather')
+export class WeatherController {
+
+    constructor(private readonly weatherService: WeatherService){}
+
+    @Get('current/:place')
+    async getUserInfo(@Param('place') place: string ){
+        return this.weatherService.getCurrentWeather(place);
+    }
+
+    
+}
---
+++ src/weather/weather.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+@Injectable()
+export class WeatherService {
+
+    async getCurrentWeather(place: string):  Promise<any>{
+
+        const options = {
+            url: 'https://weather-api167.p.rapidapi.com/api/weather/current',
+            params: {
+                place: place,
+                units: 'standard',
+                lang: 'en',
+                mode: 'json'
+            },
+            headers: {
+                'x-rapidapi-key': '8b04cab07amsh315133622000192p182a60jsne985a57d3ee2',
+                'x-rapidapi-host': 'weather-api167.p.rapidapi.com',
+                Accept: 'application/json'
+            }
+            };
+
+            try {
+                const response = await axios.request(options);
+                return response.data;
+            } catch (error) {
+                console.error(error);
+            }
+    }
+
+
+}
